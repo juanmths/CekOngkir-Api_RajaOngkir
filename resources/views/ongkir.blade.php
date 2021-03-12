e4542108bf7354eb2dcc5ba24727a7743555eb6b
juanmths <672017242@student.uksw.edu>
2021-03-13 05:01:20 +0700

    perbagus tampilan

--- resources/views/ongkir.blade.php
+++ resources/views/ongkir.blade.php
@@ -8,38 +8,68 @@
     <title>Cek Onkir</title>
 
     {{-- Bootstrap Css --}}
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-</head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
 
-<body>
+</head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Akaya+Telivigala&display=swap');
+
+    * {
+        font-family: 'Akaya Telivigala', cursive;
+    }
+
+    a:hover{
+        text-decoration: none;
+    }
+    .logo_kurir{
+        border-radius: 10px;
+    }
+
+    @media only screen and (max-width: 600px) {
+        .logo_kurir{
+            display: none;
+        }
+    }
+
+</style>
+
+<body class="bg-dark">
     {{-- Navbar --}}
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Cek Ongkir</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-            </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-info shadow">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}"><i class="fa fa-terminal"></i> Cek<strong>Ongkir</strong><br></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" target="_blank" href="https://juanmths.com/fkippeduli/">Juan Mths</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" target="_blank" href="https://www.omjuan.com/">Tutorial Programming</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" target="_blank" href="#">|</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" target="_blank" href="https://www.facebook.com/juanmatheus3798/"><i class="fa fa-facebook-square"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" target="_blank" href="https://www.instagram.com/juanmthss/"><i class="fa fa-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
-    {{-- End Navbar --}}
+     {{-- End Navbar --}}
 
-    <div class="container">
+    <div class="container" style="margin-top: 100px">
         <form action="{{ url('/') }}" method="GET">
             @csrf
             <div class="card shadow">
@@ -106,7 +136,7 @@
                     <div class="row mt-3">
                         <div class="col">
                             <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-block shadow">Cek Ongkir</button>
+                                <button type="submit" class="btn btn-info btn-block shadow">Cek Ongkir</button>
                             </div>
                         </div>
                     </div>
@@ -115,13 +145,13 @@
                             @if ($cekOngkir)
                             <table class="table table-striped">
                                 <thead>
-                                  <tr>
-                                    <th scope="col">Service</th>
-                                    <th scope="col">Deskripsi</th>
-                                    <th scope="col">Harga</th>
-                                    <th scope="col">Estimasi</th>
-                                    <th scope="col">Note</th>
-                                  </tr>
+                                    <tr>
+                                        <th scope="col">Service</th>
+                                        <th scope="col">Deskripsi</th>
+                                        <th scope="col">Harga</th>
+                                        <th scope="col">Estimasi</th>
+                                        <th scope="col">Note</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
                                     @foreach ($cekOngkir as $hasil )
@@ -132,18 +162,34 @@
                                         <td>{{$hasil['cost'][0]['etd']}}</td>
                                         <td>{{$hasil['cost'][0]['note']}}</td>
                                     </tr>
-                                @endforeach
+                                    @endforeach
                                 </tbody>
-                              </table>
-                              @else
-                              @endif
+                            </table>
+                            @else
+                            @endif
                         </div>
                     </div>
                 </div>
             </div>
-        </form>           
+        </form>
+
+        <div class="logo_kurir bg-light shadow-sm p-3" style="margin-top: 100px">
+            <div class="row justify-content-around">
+                <div class="col-sm-4"><img src="{{ asset('logo_kurir/pos.png') }}" alt="" width="50%" height="100px"></div>
+                <div class="col-sm-4"><img src="{{ asset('logo_kurir/jne.png') }}" alt="" width="50%" height="100px"></div>
+                <div class="col-sm-4"><img src="{{ asset('logo_kurir/tiki.png') }}" alt="" width="100%" height="100px"></div>
+            </div>
         </div>
+
     </div>
+
+    <!-- Akhir Contact Us -->
+
+    <!-- Footer -->
+    <footer class="text-white text-center pt-3 fixed-bottom bg-info shadow">
+      <p>Created with <i class="fa fa-coffee"></i> by <a class="text-white font-weight-bold" href="https://www.instagram.com/juanmthss/" target="_blank" title="Juan Matheus">Juan Matheus</a></p>
+    </footer>
+    <!-- Akhir Footer -->
     {{-- Bootstrap Js --}}
     <script src="https://code.jquery.com/jquery-3.6.0.js"
         integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
@@ -194,7 +240,7 @@
                     $('select[name="origin"]').empty();
                 }
             });
-            
+
         });
 
     </script>
